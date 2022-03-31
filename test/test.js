e6f68adefbfd28083e69876469a00d66360d3507
Kilros0817 <lazarkitfrl@gmail.com>
2022-03-31 19:07:18 +0300

    add test

--- test/test.js
+++ test/test.js
@@ -11,14 +11,22 @@ describe("My Test!", function() {
 
     let postID = 0;
     await MicrobloggingContract.createPost(owner.address, "This is my test post!");
-    console.log(`Credit of ${Owner.address}:`, MicrobloggingContract.users(owner.address));
+    let userCredit = await MicrobloggingContract.users(owner.address);
+    console.log(`Credit of ${owner.address} before post - null credit:`, userCredit);
     postID ++;
+    console.log(`Created post: ${postID}`, await MicrobloggingContract.posts(postID));
     let voteID = 0;
     await MicrobloggingContract.createVote(owner.address, postID, 1);
     voteID ++;
+    userCredit = await MicrobloggingContract.users(owner.address);
+    console.log(`Credit of ${owner.address} after create vote:`, userCredit);
+    console.log(`Vote of ${owner.address}:`, await MicrobloggingContract.votes(voteID));
     await MicrobloggingContract.editVote(voteID, 5);
+    userCredit = await MicrobloggingContract.users(owner.address);
+    console.log(`Credit of ${owner.address} after edit vote:`, userCredit);
+    console.log(`Vote of ${owner.address} - updated:`, await MicrobloggingContract.votes(voteID));
 
     await MicrobloggingContract.removeVote(voteID);
-
+    console.log("vote removed - null:", await MicrobloggingContract.votes(voteID));
   });
 });
